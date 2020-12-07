a937f4435d8bc0d4aa6d46c2a38633a0a12df1da
王连喜 <wanglianxi@bmsoft.com.cn>
2020-12-07 15:48:39 +0800

    迭代器模式

--- src/main/java/com/xizi/proxy_mode/ProxyModeApplication.java
+++ src/main/java/com/xizi/proxy_mode/ProxyModeApplication.java
@@ -1,6 +1,5 @@
 package com.xizi.proxy_mode;
 
-import com.xizi.proxy_mode.util.BeanUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProxyModeApplication {
 
     public static void main(String[] args) {
-        BeanUtil.applicationContext = SpringApplication.run(ProxyModeApplication.class, args);
+        SpringApplication.run(ProxyModeApplication.class, args);
     }
 
 }
---
+++ src/main/java/com/xizi/proxy_mode/restaurant/Breakfast.java
@@ -0,0 +1,9 @@
+package com.xizi.proxy_mode.restaurant;
+
+/**
+ * @author 夜尽
+ * @date 2020/12/7 15:47
+ */
+
+public class Breakfast {
+}
